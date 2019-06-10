299587edbc3635f5c493e2f1899b73eeb2224489
Tri Ardini <tr.ardn@gmail.com>
2019-06-10 21:09:50 +0700

    initial commit

---
+++ app/src/main/java/com/triardn/recyclerviewexample/Item.kt
@@ -0,0 +1,3 @@
+package com.triardn.recyclerviewexample
+
+data class Item (val name: String?, val image: Int?)
---
+++ app/src/main/java/com/triardn/recyclerviewexample/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.triardn.recyclerviewexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private var items: MutableList<Item> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val list = findViewById<RecyclerView>(R.id.club_list)
+        initData()
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this, items){
+            val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+    private fun initData(){
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+        items.clear()
+        for (i in name.indices) {
+            items.add(Item(name[i],
+                image.getResourceId(i, 0)))
+        }
+
+        //Recycle the typed array
+        image.recycle()
+    }
+}
